6693c42fa1e640d6ff5ee684b55b823640e27041
Marshall Perrin <mperrin@stsci.edu>
2015-05-01 00:33:03 -0400

    (WIP) Kolmogorov WFE class

--- poppy/wfe.py
+++ poppy/wfe.py
@@ -169,3 +169,78 @@ class ZernikeWFE(WavefrontError):
         opd_as_phase = 2 * np.pi * combined_zernikes / wave.wavelength
         zernike_wfe_phasor = np.exp(1.j * opd_as_phase)
         return zernike_wfe_phasor
+
+
+
+class StatisticalOpticalElement(WavefrontError):
+    """
+    A statistical realization of some wavefront error, computed on a fixed grid. 
+
+    This is in a sense like an AnalyticOpticalElement, in that it is in theory computable on any grid,
+    but once computed it has some fixed sampling that cannot easily be changed. 
+
+    """
+    def __init__(self, name=None,  seed=None,  **kwargs):
+        if name is None: name = "Zernikes over a circle of radius= %.1f m" % size
+        OpticalElement.__init__(self,name=name,**kwargs)
+
+        self._RandomState = numpy.random.RandomState(seed=seed)
+
+    def randomize(self,seed=None):
+        """ Create new random instance """
+
+
+class KolmogorovWFE(StatisticalOpticalElement):
+    """
+    See
+
+    http://www.opticsinfobase.org/view_article.cfm?gotourl=http%3A%2F%2Fwww%2Eopticsinfobase%2Eorg%2FDirectPDFAccess%2F8E2A4176%2DED0A%2D7994%2DFB0AC49CECB235DF%5F142887%2Epdf%3Fda%3D1%26id%3D142887%26seq%3D0%26mobile%3Dno&org=
+
+    http://optics.nuigalway.ie/people/chris/chrispapers/Paper066.pdf
+
+    """
+    def __init__(self, name=None,  seed=None, r0=15, L_inner=0.001, L_outer=10,  **kwargs):
+        if name is None: name = "Zernikes over a circle of radius= %.1f m" % size
+        StatisticalOpticalElement.__init__(self,name=name,**kwargs)
+        raise NotImplementedError('Not implemented yet')
+
+class PowerSpectralDensityWFE(StatisticalOpticalElement):
+    """ Compute WFE from a power spectral density. 
+
+    Inspired by (and loosely derived from) prop_psd_errormap in John Krist's PROPER library.
+
+
+    For some background on structure functions & why they are useful, see : http://www.optics.arizona.edu/optomech/Spr11/523L/Specifications%20final%20color.pdf
+
+    """
+    def __init__(self, name=None,  seed=None, low_freq_amp=1, correlation_length=1.0, powerlaw=1.0,  **kwargs):
+        """ 
+
+        Parameters
+        -----------
+        low_freq_amp : float
+            RMS error per spatial frequency at low spatial frequencies. 
+        correlation_length : float
+            Correlation length parameter in cycles/meter. This indicates where the PSD transitions from
+            the low frequency behavior (~ constant amplitude per spatial frequency) to the high
+            frequency behavior (~decreasing amplitude per spatial frequency)
+        powerlaw : float
+            The power law exponent for the falloff in amplitude at high spatial frequencies.
+        """
+        if name is None: name = "Power Spectral Density WFE map "
+        StatisticalOpticalElement.__init__(self,name=name,**kwargs)
+        raise NotImplementedError('Not implemented yet')
+
+        # compute X and Y coordinate grids 
+        # compute wavenumber K in cycles/meter
+        # compute 2D PSD
+        # set piston to zero
+        # scale RMS error as desired
+        # create realization of the PSD using random phases
+        # force realized map to have the desired RMS
+
+    def saveto(self, filename):
+        raise NotImplementedError('Not implemented yet')
+
+
+
